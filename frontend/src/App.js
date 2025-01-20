ebf6943558e7c58573699d6a7edf759c5f8c4560
oscareduardoorozco <oscareduardoorozco@reactiva.com.co>
2025-01-20 17:58:21 -0500

    creacion proyecto 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './routes';
+import './index.css';
 import SideNavBar from "./SideNavBar/SideNavBar";
 
 function App() {
 	return (
 		<div>
-			<SideNavBar />
+		<SideNavBar>
+		<Routes />
+			</SideNavBar >
 		</div>
 	);
 }
 
 export default App;
+
